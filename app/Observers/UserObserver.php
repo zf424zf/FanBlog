3c48800b8a34a3ec7f30246243699ee3f6fba6a5
zf424zf <704273241@qq.com>
2018-08-22 12:31:42 +0800

    add default avatar when user reg and change home page

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,11 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        if (empty($user->avatar)) {
+            $user->avatar = config('app.url') . '/upload/images/avatars/201808/15/1_1534337186_AIhyprQnBU.jpg';
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'TopicsController@index')->name('root');
 
 Auth::routes();
 
