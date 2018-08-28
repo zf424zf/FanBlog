0676f9687a044db684e64c1b5b9472911e92e64c
zf424zf <704273241@qq.com>
2018-08-29 00:12:24 +0800

    social authorizatiion

---
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Requests\Api\SocialAuthorizationRequest;
+
+class AuthorizationsController extends Controller
+{
+    public function socialStore($type, SocialAuthorizationRequest $request)
+    {
+        if (!in_array($type, ['weixin'])) {
+            return $this->response->errorBadRequest();
+        }
+
+        $driver = \Socialite::driver($type);
+//        dd($driver->getAccessTokenResponse('081Ra5xi2MKKcH08Dbvi25J0xi2Ra5x4'));
+        try {
+            if ($code = $request->code) {
+                $response = $driver->getAccessTokenResponse($code);
+                $token = array_get($response, 'access_token');
+            } else {
+                $token = $request->access_token;
+
+                if ($type == 'weixin') {
+                    $driver->setOpenId($request->openid);
+                }
+            }
+
+            $oauthUser = $driver->userFromToken($token);
+        } catch (\Exception $e) {
+            return $this->response->errorUnauthorized('参数错误，未获取用户信息');
+        }
+        switch ($type) {
+            case 'weixin':
+                $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
+
+                if ($unionid) {
+                    $user = User::where('wx_unionid', $unionid)->first();
+                } else {
+                    $user = User::where('wx_openid', $oauthUser->getId())->first();
+                }
+
+                // 没有用户，默认创建一个用户
+                if (!$user) {
+                    $user = User::create([
+                        'name' => $oauthUser->getNickname(),
+                        'avatar' => $oauthUser->getAvatar(),
+                        'wx_openid' => $oauthUser->getId(),
+                        'weixin_unionid' => $unionid,
+                    ]);
+                }
+
+                break;
+        }
+
+        return $this->response->array(['token' => $user->id]);
+    }
+}
